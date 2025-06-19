eae3a259f1b1f6b7628923a50412daa8e8e75f21
Greg Prisament <gprisament@gmail.com>
2025-06-19 12:51:18 -0700

    Implement basic DXF sections

---
+++ tarnish/src/geom.rs
@@ -0,0 +1,51 @@
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct Vec2 {
+    pub x: f64,
+    pub y: f64,
+}
+
+#[derive(Serialize)]
+pub struct Bounds2 {
+    pub min: Vec2,
+    pub max: Vec2,
+}
+
+#[derive(Serialize)]
+pub struct Line {
+    pub point: Vec2,
+    pub norm: Vec2,
+}
+
+#[derive(Serialize)]
+pub struct Rect {
+    pub ll: Vec2, // Lower left.
+    pub ur: Vec2, // Upper right.
+}
+
+#[derive(Serialize)]
+pub struct Circle {
+    pub center: Vec2,
+    pub radius: f64,
+}
+
+#[derive(Serialize)]
+pub struct Polyline {
+    pub v: Vec<Vec2>,
+}
+
+#[derive(Serialize)]
+#[allow(dead_code)]
+pub enum Shape {
+    Rect(Rect),
+    Circle(Circle),
+    Polyline(Polyline),
+}
+
+#[derive(Serialize)]
+pub struct Part {
+    pub outline: Shape,
+    pub cutouts: Vec<Shape>,
+    pub bendlines: Vec<Line>,
+}
--- tarnish/src/main.rs
+++ tarnish/src/main.rs
@@ -1,65 +1,33 @@
+// Run with: cargo run
 mod dxf;
+mod geom;
 
-use serde::Serialize;
-use std::fs::File;
-use std::io::prelude::*;
-
-#[derive(Serialize)]
-struct Vec2 {
-    x: f64,
-    y: f64,
-}
-
-#[derive(Serialize)]
-struct Line {
-    point: Vec2,
-    norm: Vec2,
-}
-
-#[derive(Serialize)]
-struct Rect {
-    ll: Vec2, // Lower left.
-    ur: Vec2, // Upper right.
-}
-
-#[derive(Serialize)]
-struct Circle {
-    center: Vec2,
-    radius: f64,
-}
-
-#[derive(Serialize)]
-struct Polyline {
-    v: Vec<Vec2>,
-}
-
-#[derive(Serialize)]
-#[allow(dead_code)]
-enum Shape {
-    Rect(Rect),
-    Circle(Circle),
-    Polyline(Polyline),
-}
-
-#[derive(Serialize)]
-struct Part {
-    outline: Shape,
-    cutouts: Vec<Shape>,
-    bendlines: Vec<Line>,
-}
+// use std::fs::File;
+// use std::io::prelude::*;
 
 fn main() -> std::io::Result<()> {
-    let p = Part {
-        outline: Shape::Rect(Rect {
-            ll: Vec2 { x: 0.0, y: 0.0 },
-            ur: Vec2 { x: 100.0, y: 100.0 },
-        }),
-        cutouts: Vec::new(),
-        bendlines: Vec::new(),
+    //let p = Part {
+    //    outline: Shape::Rect(Rect {
+    //        ll: Vec2 { x: 0.0, y: 0.0 },
+    //        ur: Vec2 { x: 100.0, y: 100.0 },
+    //    }),
+    //    cutouts: Vec::new(),
+    //    bendlines: Vec::new(),
+    //};
+    //let serialized = serde_json::to_string(&p).unwrap();
+    // let mut file = File::create("out.dxf")?;
+    // write!(file, "{}{}", dxf::HEADER, serialized)?;
+    //println!("serialized = {}", serialized);
+    let extent = geom::Bounds2 {
+        min: geom::Vec2 { x: 0.0, y: 0.0 },
+        max: geom::Vec2 {
+            x: 1000.0,
+            y: 1000.0,
+        },
     };
-    let serialized = serde_json::to_string(&p).unwrap();
-    let mut file = File::create("out.json")?;
-    write!(file, "{}{}", dxf::HEADER, serialized)?;
-    println!("serialized = {}", serialized);
+    println!("{}", dxf::gen_header(extent));
+    println!("{}", dxf::BLOCKS);
+    println!("{}", dxf::ENTITIES);
+    println!("{}", dxf::FOOTER);
     Ok(())
 }
