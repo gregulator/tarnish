25a466c4ccbfefda7bca22e034877ef292991274
Greg Prisament <gprisament@gmail.com>
2025-07-04 13:44:56 -0700

    Add frgasket_main

--- gen_all.sh
+++ gen_all.sh
@@ -3,6 +3,7 @@ D=`date "+%Y%m%d_%H%M%S"`
 mkdir -p _out_backup/$D
 mv _out/* _out_backup/$D
 (cd tarnish && cargo run --bin=wooftrim_main > ../_out/wooftrim_$D.dxf)
+(cd tarnish && cargo run --bin=frgasket_main > ../_out/frgasket_$D.dxf)
 (cd tarnish && cargo run --bin=frtrim_main > ../_out/frtrim_$D.dxf)
 (cd tarnish && cargo run --bin=woofgasket_main > ../_out/woofgasket_$D.dxf)
 (cd tarnish && cargo run --bin=stand_main > ../_out/stand_$D.dxf)
