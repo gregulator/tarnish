3a02094fec590076e26d7ddde59d8bde2fa79431
Greg Prisament <gprisament@gmail.com>
2025-07-04 04:42:56 -0700

    Add script to generate all files

---
+++ gen_all.sh
@@ -0,0 +1,8 @@
+D=`date "+%Y%m%d_%H%M%S"`
+
+mkdir -p _out_backup/$D
+mv _out/* _out_backup/$D
+(cd tarnish && cargo run --bin=wooftrim_main > ../_out/wooftrim_$D.dxf)
+(cd tarnish && cargo run --bin=woofgasket_main > ../_out/woofgasket_$D.dxf)
+(cd tarnish && cargo run --bin=stand_main > ../_out/stand_$D.dxf)
+(cd tarnish && cargo run --bin=baffle_main > ../_out/baffle_$D.dxf)
